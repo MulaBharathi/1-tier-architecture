9e5dbeb0b60c87fd2f0a1926ace0c077099efc3a
Bharathimula <bharathim273@gmail.com>
2025-07-12 16:49:13 +0530

    Modified

--- app.py
+++ app.py
@@ -3,5 +3,3 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({"message": "Hello from 1-tier flask ap on aws ec2!"})
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,11 @@ cd ~/1-tier-architecture || exit
 
 pip3 install -r requirements.txt
 
-# Kill any running Flask app
-pkill -f "python3 app.py"
+pip install gunicorn 
 
-# Run the app in the background and log output
-nohup python3 app.py > app.log 2>&1 &
+# Kill any running Flask app
+pkill gunicorn || true
+
+# Run app with gunicorn in background
+gunicorn -w 4 -b 0.0.0.0:5000 app:app --daemon
 
